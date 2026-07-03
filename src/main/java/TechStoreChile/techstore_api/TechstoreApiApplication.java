668f8499711c0cb7dadaecf5cbded4ba4fee6ab8
Oscar Astudillo <osca.astudillo@duocuc.cl>
2026-07-02 23:56:45 -0400

    fix: Se corrige SqsConfig

--- src/main/java/TechStoreChile/techstore_api/TechstoreApiApplication.java
+++ src/main/java/TechStoreChile/techstore_api/TechstoreApiApplication.java
@@ -2,12 +2,22 @@ package TechStoreChile.techstore_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sqs.SqsClient;
 
 @SpringBootApplication
 public class TechstoreApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TechstoreApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TechstoreApiApplication.class, args);
+    }
 
+    // Ponemos el Bean directamente aquí para que Spring lo encuentre sí o sí
+    @Bean
+    public SqsClient sqsClient() {
+        return SqsClient.builder()
+                .region(Region.US_EAST_1)
+                .build();
+    }
 }
